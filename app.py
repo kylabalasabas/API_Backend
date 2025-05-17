a138b9e37f9ffa68d23b4cbf11d254a331c830f6
Kyla <kyla.alarmabalasabas@gmail.com>
2025-05-17 12:24:10 +0800

    first commit

--- app.py
+++ app.py
@@ -6,17 +6,17 @@ CORS(app)
 
 @app.route('/')
 def home():
-    return "Flask backend is running."
+    return "Server is running!" 
 
 @app.route('/chat', methods=['POST'])
 def chat():
     data = request.get_json()
     user_input = data.get('message', '').lower()
 
-    if user_input == 'hi':
-        return jsonify({'response': 'hello'})
+    if user_input == 'hi':  
+        return jsonify({'response': 'Hello, Kyla!'})  
     else:
-        return jsonify({'response': f'You said: {user_input}'})
-
+        return jsonify({'response': "I don't understand."})  
+    
 if __name__ == '__main__':
     app.run(debug=True)
